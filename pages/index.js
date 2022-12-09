973feea3015760a9b0d43cc4650ac76447d3b8b4
nutv99 <nutv99@gmail.com>
2022-12-09 07:35:56 +0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to 999<a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by77 editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
